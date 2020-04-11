cc3b606a4a3c6b0e2f91c858e3ef9365c0f6093b
Chase Terry <chase.terry87@gmail.com>
2020-04-11 18:49:51 -0500

    Register components globally

--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// Import components to register them globally
+import * as components from './components'
+
+for (let componentArray of Object.entries(components)) {
+  for (let component of Object.entries(componentArray[1])) {
+    Vue.component(component[1].name, component[1])
+  }
+}
+
+export * from './components'
+// End of globally registered components
+
 Vue.config.productionTip = false
 
 new Vue({
