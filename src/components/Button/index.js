05f0225d31c6f0d9e22b7eeb3df52eb8df83e7d3
Chase Terry <chase.terry87@gmail.com>
2020-05-21 22:03:35 -0500

    Simplified component exports

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,5 +1,1 @@
-// export { default as Button } from './Button' // Why doesn't this work, but the method below does?
-
-import Button from './Button'
-
-export default Button
+export { default } from './Button'
