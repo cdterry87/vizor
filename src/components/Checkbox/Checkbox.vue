441fe72963a11ffef0b761373282426452bb0bca
Chase Terry <chase.terry87@gmail.com>
2020-05-26 20:55:58 -0500

    Started building the checkbox component.

---
+++ src/components/Checkbox/Checkbox.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <label
+      ref="label"
+      class="checkbox"
+      :class="[
+        {
+          'checkbox--rounded': rounded,
+          'checkbox--small': small,
+          'checkbox--medium': medium,
+          'checkbox--large': large,
+          'checkbox--required': required,
+          'checkbox--disabled': disabled
+        }
+      ]"
+      :disabled="disabled"
+    >
+      <input
+        ref="input"
+        type="checkbox"
+        :required="required"
+        :disabled="disabled"
+      >
+      <span class="checkbox__check" />
+      <span class="checkbox__label">
+        <slot />
+      </span>
+    </label>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'VCheckbox',
+    props: {
+      name: {
+        type: String,
+        default: 'checkbox'
+      },
+      value: {
+        type: [String, Number, Boolean],
+        default: true
+      },
+      rounded: {
+        type: Boolean,
+        default: false
+      },
+      small: {
+        type: Boolean,
+        default: false
+      },
+      medium: {
+        type: Boolean,
+        default: false
+      },
+      large: {
+        type: Boolean,
+        default: false
+      },
+      required: {
+        type: Boolean,
+        default: false
+      },
+      disabled: {
+        type: Boolean,
+        deafult: false
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  @import 'Checkbox';
+</style>
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,9 @@
 import Button from './Button'
+import Checkbox from './Checkbox'
 import Toggle from './Toggle'
 
 export {
   Button,
+  Checkbox,
   Toggle
 }
